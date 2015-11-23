6fdff14ee78d4ccddd2b871acf69e20537d6cfbc
Harsh Shandilya <msfjarvis@gmail.com>
2015-11-23 10:59:45 +0530

    Fixed bad indents
    Told you there was a problem ;)

--- script/aac_gui.py
+++ script/aac_gui.py
@@ -23,8 +23,8 @@ def init():
 		java_done = canvas.create_text(190, 170, text="Okay, java is ready to work !")
 
 	def dl_prgms():
-     os.system("curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo")
-     os.system("chmod a+x ~/bin/repo")
+     		os.system("curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo")
+     		os.system("chmod a+x ~/bin/repo")
 		os.system("sudo apt-get install git-core gnupg flex bison gperf build-essential \
   		zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
 	  	lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
