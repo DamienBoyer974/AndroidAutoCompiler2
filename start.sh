8ac4ae36e11d0b0515978a93326bd5c15fbfc20b
AndroGeek974 <damienboyer97438@gmail.com>
2015-11-23 10:16:15 +0400

    Update start.sh to manage frames

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@
 echo Start AAC...
 sudo apt-get install python3-tk
 sudo python3 script/aac_gui.py
-echo Scripted by Damien Boyer
+echo All is initialised, now click on Download Source.
+sudo python3 script/aac_gui.py
+echo Sources successfully downloaded, now click on Build Options.
+sudo python3 script/aac_gui.py
+echo If you had no errors, you ROM is successfully builded.
+echo Program end
